1d612f5e12c4370c685d675a70a9cc0914228dd4
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2025-05-20 12:49:01 +0000

    Securing REST APIs with Spring Security OAuth2 Authorization and Resource Servers

---
+++ OAuth2ClientCred-Auth-and-Res-Server/src/main/java/org/example/oauth2clientcredauthandresserver/OAuth2ClientCredAuthAndResServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.oauth2clientcredauthandresserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ClientCredAuthAndResServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ClientCredAuthAndResServerApplication.class, args);
+    }
+
+}
---
+++ OAuth2ClientCred-Auth-and-Res-Server/src/test/java/org/example/oauth2clientcredauthandresserver/OAuth2ClientCredAuthAndResServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oauth2clientcredauthandresserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ClientCredAuthAndResServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
