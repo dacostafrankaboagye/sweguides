d70119151aebf5d4bed51769499e3a1481ba2280
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2025-05-23 18:07:23 +0000

    github and google

---
+++ OAuth2Recipes/oauth-client-demo/src/main/java/org/example/oauthclientdemo/controller/HelloController.java
@@ -0,0 +1,30 @@
+package org.example.oauthclientdemo.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // for google only
+    /*
+    @GetMapping
+    public String hello(@AuthenticationPrincipal OidcUser oidcUser) {
+        return """
+                Hello %s\s
+                Your email is %s\s
+                Thank you""".formatted(oidcUser.getFamilyName(), oidcUser.getEmail());
+    }
+    */
+
+    // for github* and google
+    @GetMapping
+    public String hello(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        return """
+                Hello %s\s
+                Thank you""".formatted(oAuth2User.getAttributes());
+    }
+}
---
+++ OAuth2Recipes/oauth-client-demo/src/test/java/org/example/oauthclientdemo/OauthClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oauthclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
