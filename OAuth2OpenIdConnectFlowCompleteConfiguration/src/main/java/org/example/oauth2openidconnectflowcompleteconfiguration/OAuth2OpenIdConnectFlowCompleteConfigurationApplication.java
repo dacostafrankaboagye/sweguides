4ddd0874a6bd1669721e5271f64152fbc3c57bc6
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2025-05-23 15:50:45 +0000

    oauth2login

---
+++ OAuth2OpenIdConnectFlowCompleteConfiguration/src/main/java/org/example/oauth2openidconnectflowcompleteconfiguration/OAuth2OpenIdConnectFlowCompleteConfigurationApplication.java
@@ -0,0 +1,13 @@
+package org.example.oauth2openidconnectflowcompleteconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2OpenIdConnectFlowCompleteConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2OpenIdConnectFlowCompleteConfigurationApplication.class, args);
+    }
+
+}
